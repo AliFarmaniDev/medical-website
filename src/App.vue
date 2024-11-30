ba4c029e1b5ed0e07da22b90462fa4ac3d2ffe2c
Alireza <alirezafarmani619@gmail.com>
2024-11-30 09:53:39 -0800

    format the code and end project

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import { RouterView } from 'vue-router'
 //import header and footer components
 import Header from './components/Header.vue'
-import Footer from './components/Footer.vue';
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
